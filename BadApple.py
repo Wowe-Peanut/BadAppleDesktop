0cfbe4f857b62545bc8cd281efe8138a8d877c8a
Ryan <47678647+Wowe-Peanut@users.noreply.github.com>
2022-08-15 02:48:42 -0500

    Frames

--- BadApple.py
+++ BadApple.py
@@ -2,12 +2,19 @@ import os
 import shutil
 import cv2
 
-frames = []
-video = cv2.VideoCapture(r"badapple.mp4")
-success, image = video.read()
 
-while success:
-    frames.append(image)
+#Never call again lol
+def parse_frames:
+  video = cv2.VideoCapture('badapple.mp4')
+  success,image = video.read()
+  count = 0
+  
+  while success:
+    cv2.imwrite(r"C:\Users\Peanu\OneDrive\Desktop\BadAppleDesktop\frames\frame%d.jpg" % count, image)     # save frame as JPEG file      
     success,image = video.read()
+    count += 1
 
-cv2.imwrite("frame2.jpg", frames[100])
+
+
+
+
